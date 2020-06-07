2b477d8e51f058f70b61d8006c1b7c9ae55fce5c
nguen1225 <nguen1225@gmail.com>
2020-06-07 02:02:39 +0000

    ジャンルの新規登録を追加

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,7 +1,32 @@
 class GenresController < ApplicationController
   def index
+  	@genres = Genre.all
+  	@genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.user_id = current_user.id
+
+    if @genre.save
+      redirect_to genres_path, notice: 'Genre was successfully created.'
+    else
+      render :index
+      @genres = Genre.all
+    end
   end
 
   def edit
   end
+
+  def destroy
+  	@genre = Genre.find(params[:id])
+    @genre.destroy
+      redirect_to genres_url, notice: 'Genre was successfully destroyed.'
+  end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
 end
