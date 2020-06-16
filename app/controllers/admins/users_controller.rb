9734bbda12c66f493690d9a624db3b3c219644c6
nguen1225 <nguen1225@gmail.com>
2020-06-16 13:02:38 +0000

    検索の絞り込み修正、非同期バグ修正

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,4 +1,5 @@
 class Admins::UsersController < ApplicationController
 	def index
+		@events = User.all
 	end
 end
--- app/controllers/users/searchs_controller.rb
+++ app/controllers/users/searchs_controller.rb
@@ -1,10 +1,7 @@
 class Users::SearchsController < ApplicationController
 	def searchs
-		@search = Event.search(params[:q])
+		@search = current_user.events.search(params[:q])
 		@events = @search.result
       	@genres = Genre.where(user_id: current_user.id)
-      	if @events != current_user.id
-          redirect_to root_path(current_user)
-    	end
 	end
 end
