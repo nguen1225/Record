65db1b78164e2188aa8f43867c616fde1e0b9ff7
nguen1225 <61786366+nguen1225@users.noreply.github.com>
2020-06-30 23:39:15 +0900

    Update notifications_controller.rb

--- app/controllers/users/notifications_controller.rb
+++ app/controllers/users/notifications_controller.rb
@@ -6,8 +6,8 @@ class Users::NotificationsController < ApplicationController
 	  @genres_list = Genre.where(user_id: current_user.id)
 	  beginning_of_tomorrow = Time.now.tomorrow.beginning_of_day
 	  end_of_tomorrow = Time.now.tomorrow.end_of_day
-      @notifications = current_user.events.where("start_date >= ? and start_date <= ?  and alarm = ?", 
-    											  beginning_of_tomorrow, end_of_tomorrow, true)
+          @notifications = current_user.events.where("start_date >= ? and start_date <= ?  and alarm = ?", 
+    						      beginning_of_tomorrow, end_of_tomorrow, true)
 	end
 
 
@@ -16,8 +16,8 @@ class Users::NotificationsController < ApplicationController
   	  beginning_of_tomorrow = Time.now.tomorrow.beginning_of_day
 	  end_of_tomorrow = Time.now.tomorrow.end_of_day
   	  @notifications = current_user.events.where("start_date >= ? and start_date <= ?  and alarm = ?", 
-  												  beginning_of_tomorrow, end_of_tomorrow, true)
-      @notifications.destroy_all
-      redirect_to notifications_path
-    end
+  						      beginning_of_tomorrow, end_of_tomorrow, true)
+          @notifications.destroy_all
+          redirect_to notifications_path
+        end
 end
